08b24b8cc249ac46001d9b69ca345a95349c8e74
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2025-02-18 01:38:54 +0600

    JWT Token set up

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-console.log("user", process.env.DB_USER, "pass", process.env.DB_PASS)
+console.log("user", process.env.DB_USER, "pass", process.env.DB_PASS);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.42yqa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -20,7 +20,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -28,43 +28,97 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const usersCollection = client.db('logixshuvoDB').collection('users');
+    const usersCollection = client.db("logixshuvoDB").collection("users");
+
+    // JWT RELATED API
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+
+    // MIDDLEWARES
+    const verifyToken = (req, res, next) => {
+      console.log("inside verify token", req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorized access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.send(401).send({ message: "unauthorized access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+      // next();
+    };
 
     // GET ALL USERS DATA
-    app.get('/users', async(req, res) => {
-        const result = await usersCollection.find().toArray();
-        res.send(result);
-    })
+    app.get("/users", verifyToken, async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
 
     // REGISTER A NEW USER
-    app.post('/users', async(req, res) =>{
-        const {name, email, role} = req.body;
+    app.post("/users", async (req, res) => {
+      const { name, email, role } = req.body;
 
-        // check if the user exist or not
-        const existingUser = await usersCollection.findOne({email});
-        if(existingUser){
-            return res.send({ message: "User Already exists", insertedId: null });
-        }
+      // check if the user exist or not
+      const existingUser = await usersCollection.findOne({ email });
+      if (existingUser) {
+        return res.send({ message: "User Already exists", insertedId: null });
+      }
 
-        // insert new user
-        const newUser = {name, email, role};
-        const result = await usersCollection.insertOne(newUser);
-        res.send(result);
-    })
+      // insert new user
+      const newUser = { name, email, role };
+      const result = await usersCollection.insertOne(newUser);
+      res.send(result);
+    });
 
     // CHANGE USER ROLE
+    app.patch("/users/role/:id", async (req, res) => {
+      const id = req.params.id;
+      const { role } = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: { role },
+      };
+      const result = await usersCollection.updateOne(filter, updatedDoc);
+      if (result.modifiedCount > 0) {
+        res.send({ success: true, modifiedCount: result.modifiedCount });
+      } else {
+        res.send({ success: false, message: "No changes made" });
+      }
+    });
+
+    // VERIFY ADMIN
+    // const verifyAdmin = async (req, res, next) => {
+    //   const email = req.decoded.email;
+    //   const query = { email: email };
+    //   const user = await userCollection.findOne(query);
+    //   const isAdmin = user?.role === "admin";
+    //   if (!isAdmin) {
+    //     return res.status(403).send({ message: "forbidden access" });
+    //   }
+    //   next();
+    // };
 
     // DELETE A USER
-    app.delete('/users/:id', async(req, res) => {
+    app.delete("/users/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) };
       const result = await usersCollection.deleteOne(query);
       res.send(result);
-    })
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -72,11 +126,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-    res.send('logixshuvo server is running ')
-})
+app.get("/", (req, res) => {
+  res.send("logixshuvo server is running ");
+});
 
 app.listen(port, () => {
-    console.log(`LogixShuvo server is running on port: ${port}`)
-})
+  console.log(`LogixShuvo server is running on port: ${port}`);
+});
